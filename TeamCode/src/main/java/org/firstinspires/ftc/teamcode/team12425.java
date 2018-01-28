da377eeb7d5d04b1c4744a6193a4093920a31a0b
DESKTOP-NHTJCLA\angel <Angel142312@gmail.com>
2018-01-28 18:08:22 -0500

    dgs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team12425.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team12425.java
@@ -13,47 +13,87 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name = "12425" , group = "12425")
 public class team12425 extends LinearOpMode
 {
-   private DcMotor motortopLeft;
-    private DcMotor motorbottomLeft
-    private DcMotor motortopRight
-    private DcMotor motorbottomRight
+    private DcMotor motorFrontLeft;
+    private DcMotor motorFrontRight;
+    private DcMotor motorBackLeft;
+    private DcMotor motorBackRight;
 
-   private Servo armServo;
+   private Servo leftArm;
+    private Servo rightArm;
+    private Servo lift1;
+    private Servo lift2;
+    private Servo armExt;
 
-   private  static final double ARM_RETRACTED_POSITION = 0.2
-    private  static final double ARM_EXTENDED_POSITION = 0.8
+   private  static final double ARM_RETRACTED_POSITION = 0.2;
+    private  static final double ARM_EXTENDED_POSITION = 0.8;
 
     @Override
     public void runOpsMode () throws  InterruptedException
     {
-        motorleft = hardwareMap.dcMotor.get ("motorLeft");
-        motorRight = hardwareMap.dcMotor.get ("motorRight");
+        motorFrontLeft = hardwareMap.dcMotor.get ("motorFrontLeft");
+        motorFrontRight = hardwareMap.dcMotor.get ("motorFrontRight");
+        motorBackLeft = hardwareMap.dcMotor.get ("motorBackLeft");
+        motorBackRight = hardwareMap.dcMotor.get ("motorBackRight");
 
-        motorleft.setDirection(DcMotor.Direction.REVERSE);
+        motorFrontLeft.setDirection(DcMotor.Direction.REVERSE);
+        motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
 
-        armServo = hardwareMap.servo.get ("armServo");
+        leftArm = hardwareMap.servo.get ("leftArm");
+        rightArm = hardwareMap.servo.get ("rightArm");
 
-        armServo.setPosition(ARM_RETRACTED_POSITION);
+        leftArm.setDirection(Servo.Direction.REVERSE);
 
+        lift1 = hardwareMap.servo.get("lift1");
+        lift2 = hardwareMap.servo.get("lift2");
+
+        armExt = hardwareMap.servo.get("armExt");
 
         waitForStart();
 
         while (opModeIsActive())
         {
-            motorleft.setPower(-gamepad1.left_stick_y);
-            motorRight.setPower(-gamepad1.right_stick_y);
+            motorFrontLeft.setPower(-gamepad1.left_stick_y);
+            motorBackLeft.setPower(-gamepad1.left_stick_y);
+
+            motorFrontRight.setPower(-gamepad1.right_stick_y);
+            motorBackRight.setPower(-gamepad1.right_stick_y);
 
             if (gamepad2.a)
             {
-                armServo.setPosition(ARM_EXTENDED_POSITION);
+                leftArm.setPosition(ARM_EXTENDED_POSITION);
+                rightArm.setPosition(ARM_EXTENDED_POSITION);
             }
 
             if (gamepad2.b)
             {
-                armServo.setPosition(ARM_RETRACTED_POSITION);
+                leftArm.setPosition(ARM_RETRACTED_POSITION);
+                rightArm.setPosition(ARM_RETRACTED_POSITION);
             }
 
-           idle();
+            if (gamepad2.left_stick_y > 0)
+            {
+                lift1.setPosition(0.5);
+                lift2.setPosition(0.5);
+            }
+
+            if (gamepad2.left_stick_y < 0)
+            {
+                lift1.setPosition(0.2);
+                lift2.setPosition(0.2);
+            }
+
+            if (gamepad2.x)
+            {
+                armExt.setPosition(0.5);
+            }
+
+            if (gamepad2.y)
+            {
+                armExt.setPosition(0.2);
+            }
+
+
+            idle();
         }
     }
 
